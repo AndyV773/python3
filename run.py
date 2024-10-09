28a23f7ef45e01c718d4f9a87c80c85d623f46aa
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-09 19:54:08 +0000

    Add golden egg function

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def number_input():
     """
     
     """
-    amount_of_numbers = input("Enter the amount of number:\n")
+    amount_of_numbers = input("Enter the amount of numbers:\n")
     return int(amount_of_numbers)
 
 def range_input():
@@ -25,6 +25,18 @@ def range_input():
     return int(range_num)
 
 
+def golden_egg(i):
+    """
+    
+    """
+    for j in i:
+        if j == 7:
+            print("You won a golden egg ()")
+            break
+    else:
+        print("Better luck next time...\n")
+
+
 def run_program():
     """
     
@@ -32,6 +44,8 @@ def run_program():
     while True:
         test = random_numbers(number_input(), range_input())
         print(test)
+        golden_egg(test)
+
         z = input("Would you like to continue? y/n \n")
 
         if z.lower() == "y":
