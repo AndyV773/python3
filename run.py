9ffe6c38ef76327272a10cd21c0368cbb7ded93c
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-09 19:00:31 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def random_numbers(num):
+    """
+    Creates a random list of numbers in range of argument input
+    """
+    x = []
+    for y in range(num):
+        x.append(round(random.random() * 100))
+    return x
+
+test = random_numbers(20)
+print(test)
