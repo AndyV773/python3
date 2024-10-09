b191dc2fe49a743a64ce4f08e75ddc13fc5f50f6
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-09 19:42:59 +0000

    Created some extra functions

--- run.py
+++ run.py
@@ -1,13 +1,43 @@
 import random
 
-def random_numbers(num):
+def random_numbers(num, range_num):
     """
     Creates a random list of numbers in range of argument input
     """
     x = []
     for y in range(num):
-        x.append(round(random.random() * 100))
+        x.append(round(random.random() * range_num))
     return x
 
-test = random_numbers(20)
-print(test)
+
+def number_input():
+    """
+    
+    """
+    amount_of_numbers = input("Enter the amount of number:\n")
+    return int(amount_of_numbers)
+
+def range_input():
+    """
+    
+    """
+    range_num = input("Enter number range:\n")
+    return int(range_num)
+
+
+def run_program():
+    """
+    
+    """
+    while True:
+        test = random_numbers(number_input(), range_input())
+        print(test)
+        z = input("Would you like to continue? y/n \n")
+
+        if z.lower() == "y":
+            continue
+        else:
+            print("Exiting...")
+            break
+
+run_program()
