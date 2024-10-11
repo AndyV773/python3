c9b8f2c13c85308537bfd6c92b2c479196f0a772
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-11 08:33:52 +0000

    Add abc.txt

--- run.py
+++ run.py
@@ -31,10 +31,12 @@ def golden_egg(i):
     """
     for j in i:
         if j == 7:
-            print("You won a golden egg ()")
+            print("You won a golden egg ()\n")
+            print(f"Total: {sum(i)} \n")
             break
     else:
         print("Better luck next time...\n")
+        print(f"Total: {sum(i)} \n")
 
 
 def run_program():
@@ -43,15 +45,51 @@ def run_program():
     """
     while True:
         test = random_numbers(number_input(), range_input())
-        print(test)
+        print(f"{test}\n")
         golden_egg(test)
 
-        z = input("Would you like to continue? y/n \n")
+        odd_even(test)
 
-        if z.lower() == "y":
-            continue
-        else:
+        z = input("Enter any key to continue, or 'e' to exit.. \n")
+
+        if z.lower() == "e":
             print("Exiting...")
             break
+        else:
+            continue
 
-run_program()
+
+class AClass:
+    """
+    
+    """
+    def __init__(self, f):
+        self.f
+
+
+def odd_even(nums):
+    """
+    
+    """
+    odd = []
+    even = []
+    for num in nums:
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odd.append(num)
+    
+    print(odd)
+    print(even)
+    
+    amount = (sum(odd) * 10) / sum(even)
+    print(f"({sum(odd)} * 10) / {sum(even)} = {amount}")
+
+
+def main():
+    """
+    
+    """
+    run_program()
+
+main()
